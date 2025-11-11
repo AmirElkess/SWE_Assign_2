e5d2cb6fd073eae3126a8272f8928056ee77318a
AmirElkess <amir.elkess@gmail.com>
2025-11-11 22:54:18 +0100

    implement getRecommendedComputerTypePerBudget method

--- task2/src/main/java/at/aau/ComputerRecommender.java
+++ task2/src/main/java/at/aau/ComputerRecommender.java
@@ -2,12 +2,25 @@ package at.aau;
 
 public class ComputerRecommender {
     /* Equivalence Partitions
-     * 1-5;A
-     * 6-INF;B
-     * ...
+     * -INF-0    ;InvalidBudgetException
+     * 1-450     ;ENTRYLVL
+     * 451-600   ;BDGTPC
+     * 601-1500  ;GAMINGPC
+     * 1501-INF  ;ENTHUSIASTPC
      */
-    public ComputerType getRecommendedComputerTypePerBudget(int maxBudget) {
-        //TODO implement
-        return null;
+    public ComputerType getRecommendedComputerTypePerBudget(int maxBudget) throws InvalidBudgetException {
+        if (maxBudget <= 0) {
+            throw new InvalidBudgetException("Budget must be a positive integer");
+        }
+        if (maxBudget <= 450) {
+            return ComputerType.ENTRYLVL;
+        }
+        if (maxBudget <= 600) {
+            return ComputerType.BDGTPC;
+        }
+        if (maxBudget <= 1500) {
+            return ComputerType.GAMINGPC;
+        }
+        return ComputerType.ENTHUSIASTPC;
     }
 }
