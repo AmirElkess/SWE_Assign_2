2530d2b0be4444c037cf0905e63dd26e6b3a60a8
AmirElkess <amir.elkess@gmail.com>
2025-11-13 03:00:21 +0100

    add failing test for MyCollection.remove() method.

--- task2_3_4_5/src/test/java/at/aau/MyCollectionTest.java
+++ task2_3_4_5/src/test/java/at/aau/MyCollectionTest.java
@@ -2,10 +2,20 @@ package at.aau;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyCollectionTest {
     @Test
-    void name() {
+    void removeItemTest() {
+        MyCollection coll = new MyCollection(3);
+        coll.add("a");
+        coll.add("b");
+        coll.add("c");
 
+        assertEquals(3, coll.size());
 
+        // remove an element then check size is reduced
+        coll.remove("b");
+        assertEquals(2, coll.size());
     }
 }
