60edd2a90fc80074e55bf32cc588d46bd962c5d3
AmirElkess <amir.elkess@gmail.com>
2025-11-13 03:02:08 +0100

    implement MyCollection.remove() method.

--- task2_3_4_5/src/main/java/at/aau/MyCollection.java
+++ task2_3_4_5/src/main/java/at/aau/MyCollection.java
@@ -2,15 +2,16 @@ package at.aau;
 
 public class MyCollection {
     private String[] list;
-    private int cursor=0;
+    private int cursor = 0;
 
-    public MyCollection(int capacity){
+    public MyCollection(int capacity) {
         list = new String[capacity];
-        cursor=0;
+        cursor = 0;
     }
 
     /**
      * Returns the size of the collection
+     *
      * @return The number of instances in the collection
      */
     public int size() {
@@ -19,10 +20,11 @@ public class MyCollection {
 
     /**
      * Adds the String from to list. If the list is full it throws an IllegalArgumentException
+     *
      * @param s String to remove
      */
     public void add(String s) {
-        list[cursor++]=s;
+        list[cursor++] = s;
     }
 
     /**
@@ -30,9 +32,25 @@ public class MyCollection {
      * IllegalArgumentException. If the list is empty it throws an IllegalArgumentException
      * @param s String to remove
      */
-    public void remove(String s) throws IllegalArgumentException{
+    public void remove(String s) throws IllegalArgumentException {
+        if (cursor == 0) {
+            throw new IllegalArgumentException("Collection is empty");
+        }
 
+        for (int i = 0; i < cursor; i++) {
+            if (java.util.Objects.equals(list[i], s)) {
+                for (int j = i; j < cursor - 1; j++) {
+                    list[j] = list[j + 1];
+                }
+                list[cursor - 1] = null;
+                cursor--;
+                return;
+            }
+        }
+
+        throw new IllegalArgumentException("Element not found: " + s);
     }
+
 
     /**
      * Removes all items from the list and initializes a new list
